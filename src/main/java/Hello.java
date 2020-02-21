1da5dcc3f91ae9d43830d51481260a3d2225b723
AndiswaMkhonto <victoriousmkhonto@gmail.com>
2020-02-21 08:07:37 +0200

    Level1Katas Project

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,107 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hello {
-    private String name;
-
-//    public Hello() {
-//
-//    }
-
-    public String getName() {
-        return name;
+    public void hello(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=sc.next();
+        String greeting = "Hello" +" "+name;
+        System.out.println(greeting);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    //Function to check if a number is odd or even
-    public void odd_or_even() {
-        int x;
-        System.out.println("Enter an integer to check if it's odd or even");
-        Scanner in = new Scanner(System.in);
-        x = in.nextInt();
-
-        if (x % 2 == 0)
-            System.out.println("The number is even.");
-        else
-            System.out.println("The number is odd.");
-    }
-
-    //A function to draw a square according to an entered integer
-    public void square() {
-        int y;
-        System.out.println("Enter an integer to draw a square");
-        Scanner in = new Scanner(System.in);
-        y = in.nextInt();
-        for(int i=0; i<y;i++){
-            for(int j=0; j<y;j++){
-                System.out.print("#");
-            }
-            System.out.println();
-        }
-    }
-
-    //A function to draw a right angled triangle
-    public void rightAngledTriangle() {
-        int z;
-        System.out.println("Enter an integer to draw a right angled triangle");
-        Scanner in = new Scanner(System.in);
-        z = in.nextInt();
-
-        for(int i=0; i<z; i++){
-            for(int j=0; j<i+1;j++){
-                System.out.print("#");
-            }
-            System.out.println();
-        }
-
-    }
-    //A function to draw an isosceles triangle
-    public void isoscelesTriangle(int size) {
-        System.out.println("Enter an integer to draw an isosceles triangle");
-        Scanner in = new Scanner(System.in);
-        size = in.nextInt();
-
-        for(int row=1; row<=size;row++){
-            //for spacing on both sides of columns
-            for(int colSpace=size-row;colSpace>=1;colSpace--){
-                System.out.print(" ");
-            }
-            for(int colHashes=1;colHashes<=(row*2)-1;colHashes++){
-                System.out.print("#");
-            }
-            System.out.println();
-        }
-//
-    }
-    public void findLongestString(){
-        String[] array=new String[]{"once", "upon", "a", "time"};
-        int lengthElement = array[0].length();
-        for(int i=0;i<array.length;i++) {
-                if(array[i].length() >= lengthElement) {
-
-                    lengthElement=array[i].length();
-                }
-        }
-        for(int i=0;i<array.length;i++) {
-            if(lengthElement == array[i].length())
-                System.out.println(array[i]);
-        }
-    }
-
-    public void combineStrings(){
-        int[] arr1={11,22,33};
-        int[] arr2={1,2,3};
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<arr1.length;i++){
-            list.add(arr1[i]);
-            for(int j=i; j<=i; j++){
-                list.add(arr2[j]);
-            }
-        }
-        System.out.println(list);
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Hello h=new Hello();
-        h.setName("Tshepo");
-        String greeting = "Hello" +" "+h.getName();
-        System.out.println(greeting);
+       Hello h=new Hello();
+       h.hello();
 
         //..................................................................//
-        h.odd_or_even();
+        EvenOrOdd evenorodd = new EvenOrOdd();
+        evenorodd.odd_or_even();
         System.out.println("..............................................");
-        h.square();
+
+
+        Square s=new Square();
+        s.square();
         System.out.println("..............................................");
-        h.rightAngledTriangle();
+
+        RightHandedTriangle r=new RightHandedTriangle();
+        r.rightAngledTriangle();
         System.out.println("..............................................");
-        h.isoscelesTriangle(5);
+
+        IsoscelesTriangle i=new IsoscelesTriangle();
+        i.isoscelesTriangle(5);
         System.out.println("................................................");
-        h.findLongestString();
+
+        LongestString l=new LongestString();
+        l.findLongestString();
         System.out.println(".................................................");
-        h.combineStrings();
+
+        CombineArrays c=new CombineArrays();
+        c.combineStrings();
+
+        FrameText f=new FrameText();
+     Scanner sca = new Scanner(System.in);
+     int size = 0;
+     System.out.println("How many words you want to enter?: ");
+     size = Integer.parseInt(sca.nextLine());
+     String[] inputs = new String[size];
+     System.out.println("Now enter " + size + "Words");
+
+     for (int j = 0; j < size; j++) {
+      inputs[j] = sca.nextLine();
+     }
+
+     System.out.print("*");
+        f.frameSomeText(inputs);
+     //f.frameSomeText(new String []{"Writers", "good","code","every","day"});
     }
 }
